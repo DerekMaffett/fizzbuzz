b051c330be0ec843a3a5d8590ffc2c10d83e20a5
Derek Maffett <derekgmaffett@gmail.com>
2014-09-15 16:18:21 -0700

    robustify test suite

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,19 +1,32 @@
 require_relative "../fizzbuzz.rb"
 
 describe Fizzbuzz do 
+	fizz_values = [3, 6, 9, 12, 18, 21, 24, 333]
+	buzz_values = [5, 10, 20, 25, 100, 1075]
+	fizzbuzz_values = [15, 30, 45, 150]
+	return_input_values = [1, 2, 11, 53, 74]
+
 	it "should return 'Fizz' for multiples of three" do
-		expect(Fizzbuzz.analyze(3)).to eq("Fizz")
+		fizz_values.each do |input|
+			expect(Fizzbuzz.analyze(input)).to eq("Fizz")	
+		end
 	end 
 
 	it "should return 'Buzz' for multiples of five" do
-		expect(Fizzbuzz.analyze(5)).to eq("Buzz")
+		buzz_values.each do |input|
+			expect(Fizzbuzz.analyze(input)).to eq("Buzz")	
+		end
 	end
 
 	it "should return 'Fizzbuzz' for multiples of both five and three" do
-		expect(Fizzbuzz.analyze(15)).to eq("FizzBuzz")
+		fizzbuzz_values.each do |input|
+			expect(Fizzbuzz.analyze(input)).to eq("FizzBuzz")		
+		end
 	end
 
 	it "should return the input if not divisible by three or five" do
-		expect(Fizzbuzz.analyze(11)).to eq(11)
+		return_input_values.each do |input|
+			expect(Fizzbuzz.analyze(input)).to eq(input)			
+		end
 	end
 end
