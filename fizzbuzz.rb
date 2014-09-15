707fafe3001d526c7c7c92a705c1f00c7ed0a0e6
Derek Maffett <derekgmaffett@gmail.com>
2014-09-15 16:12:25 -0700

    refactor fizzbuzz.rb code

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,9 @@
 class Fizzbuzz
 	def self.analyze(input)
-		return "FizzBuzz" if input % 3 == 0 && input % 5 == 0
-		return "Fizz" if input % 3 == 0
-		return "Buzz" if input % 5 == 0
-		return input
+		return input if !(input % 3 == 0 || input % 5 == 0)
+		output = ""
+		output += "Fizz" if input % 3 == 0
+		output += "Buzz" if input % 5 == 0
+		return output
 	end
 end
