778f39b640c758e7e1d208b2263c17ba6b8ad995
Derek Maffett <derekgmaffett@gmail.com>
2014-09-15 15:43:03 -0700

    implement full behavior

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,8 @@
 class Fizzbuzz
-
+	def self.analyze(input)
+		return "FizzBuzz" if input % 3 == 0 && input % 5 == 0
+		return "Fizz" if input % 3 == 0
+		return "Buzz" if input % 5 == 0
+		return input
+	end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,19 @@
 require_relative "../fizzbuzz.rb"
 
 describe Fizzbuzz do 
-	it "insert this test" 
+	it "should return 'Fizz' for multiples of three" do
+		expect(Fizzbuzz.analyze(3)).to eq("Fizz")
+	end 
+
+	it "should return 'Buzz' for multiples of five" do
+		expect(Fizzbuzz.analyze(5)).to eq("Buzz")
+	end
+
+	it "should return 'Fizzbuzz' for multiples of both five and three" do
+		expect(Fizzbuzz.analyze(15)).to eq("FizzBuzz")
+	end
+
+	it "should return the input if not divisible by three or five" do
+		expect(Fizzbuzz.analyze(11)).to eq(11)
+	end
 end
