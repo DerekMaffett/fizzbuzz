2aaf4df75a381c69d73566120b66fc340a58ab2e
Derek Maffett <derekgmaffett@gmail.com>
2014-09-16 19:21:54 -0700

    refactor using RuboCop

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,10 @@
+# This class functions as a Fizzbuzz calculator.
 class Fizzbuzz
-	def self.analyze(input)
-		return input if !(input % 3 == 0 || input % 5 == 0)
-		output = ""
-		output += "Fizz" if input % 3 == 0
-		output += "Buzz" if input % 5 == 0
-		return output
-	end
+  def self.analyze(input)
+    return input unless input % 3 == 0 || input % 5 == 0
+    output = ''
+    output += 'Fizz' if input % 3 == 0
+    output += 'Buzz' if input % 5 == 0
+    output
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,32 +1,32 @@
-require_relative "../fizzbuzz.rb"
+require_relative '../fizzbuzz.rb'
 
-describe Fizzbuzz do 
-	fizz_values = [3, 6, 9, 12, 18, 21, 24, 333]
-	buzz_values = [5, 10, 20, 25, 100, 1075]
-	fizzbuzz_values = [15, 30, 45, 150]
-	return_input_values = [1, 2, 11, 53, 74]
+describe Fizzbuzz do
+  fizz_values = [3, 6, 9, 12, 18, 21, 24, 333]
+  buzz_values = [5, 10, 20, 25, 100, 1075]
+  fizzbuzz_values = [15, 30, 45, 150]
+  return_input_values = [1, 2, 11, 53, 74]
 
-	it "should return 'Fizz' for multiples of three" do
-		fizz_values.each do |input|
-			expect(Fizzbuzz.analyze(input)).to eq("Fizz")	
-		end
-	end 
+  it 'should return "Fizz" for multiples of three' do
+    fizz_values.each do |input|
+      expect(Fizzbuzz.analyze(input)).to eq('Fizz')
+    end
+  end
 
-	it "should return 'Buzz' for multiples of five" do
-		buzz_values.each do |input|
-			expect(Fizzbuzz.analyze(input)).to eq("Buzz")	
-		end
-	end
+  it 'should return "Buzz" for multiples of five' do
+    buzz_values.each do |input|
+      expect(Fizzbuzz.analyze(input)).to eq('Buzz')
+    end
+  end
 
-	it "should return 'Fizzbuzz' for multiples of both five and three" do
-		fizzbuzz_values.each do |input|
-			expect(Fizzbuzz.analyze(input)).to eq("FizzBuzz")		
-		end
-	end
+  it 'should return "Fizzbuzz" for multiples of both five and three' do
+    fizzbuzz_values.each do |input|
+      expect(Fizzbuzz.analyze(input)).to eq('FizzBuzz')
+    end
+  end
 
-	it "should return the input if not divisible by three or five" do
-		return_input_values.each do |input|
-			expect(Fizzbuzz.analyze(input)).to eq(input)			
-		end
-	end
+  it 'should return the input if not divisible by three or five' do
+    return_input_values.each do |input|
+      expect(Fizzbuzz.analyze(input)).to eq(input)
+    end
+  end
 end
