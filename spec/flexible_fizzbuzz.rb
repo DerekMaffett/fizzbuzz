f1cd12b73490e01c459602b14e734d7b9d4ef54a
Derek Maffett <derekgmaffett@gmail.com>
2014-09-22 13:09:26 -0700

    add guard and testing framework for flexible_fizzbuzz

---
+++ spec/flexible_fizzbuzz.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Flexible_fizzbuzz do
+  it 'should return "fizz" for multiples of 3'
+
+  it 'should return "buzz" for multiples of 5'
+
+  it 'should return "sivv" for multiples of 7'
+
+  it 'should return "fizzbuzz" for multiples of both 3 and 5'
+
+  it 'should return "buzzsivv" for multiples of both 5 and 7'
+
+  it 'should return "fizzbuzzsivv" for multiples of 3, 5, and 7'
+
+end
