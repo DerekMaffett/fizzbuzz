5596365711e3e201d30a1d40024a4f3ff614b8ad
Derek Maffett <derekgmaffett@gmail.com>
2014-10-02 17:11:29 -0700

    change fizzbuzzsivv to object-oriented solution

--- lib/fizzbuzzsivv.rb
+++ lib/fizzbuzzsivv.rb
@@ -1,21 +1,20 @@
 class Fizzbuzzsivv
-  def self.analyze(num, path)
-    @fizzbuzz_values = Fizzbuzzsivv.parse_rules(path)
-    @result = ''
-    @fizzbuzz_values.each do |k, v|
-      @result += v if num % k.to_i == 0
-    end
-    @result.empty? ? num : @result
-  end
-
-  def self.parse_rules(path)
+  def initialize(path)
     File.open(path) do |file|
       lines = file.readlines
       filtered_lines = lines.map do |line|
         line.gsub!("\n", '')
         line.split(', ')
       end
-      Hash[filtered_lines]
+      @fizzbuzz_values = Hash[filtered_lines]
     end
+  end
+
+  def analyze(num)
+    @result = ''
+    @fizzbuzz_values.each do |k, v|
+      @result += v if num % k.to_i == 0
+    end
+    @result.empty? ? num : @result
   end
 end
--- spec/fizzbuzzsivv_spec.rb
+++ spec/fizzbuzzsivv_spec.rb
@@ -6,34 +6,34 @@ describe Fizzbuzzsivv do
   end
 
   it 'should return "fizz" for multiples of 3' do
-    expect(Fizzbuzzsivv.analyze(3, @test_path)).to eq 'fizz'
+    expect(Fizzbuzzsivv.new(@test_path).analyze(3)).to eq 'fizz'
   end
 
   it 'should return "buzz" for multiples of 5' do
-    expect(Fizzbuzzsivv.analyze(5, @test_path)).to eq 'buzz'
+    expect(Fizzbuzzsivv.new(@test_path).analyze(5)).to eq 'buzz'
   end
 
   it 'should return "sivv" for multiples of 7' do
-    expect(Fizzbuzzsivv.analyze(7, @test_path)).to eq 'sivv'
+    expect(Fizzbuzzsivv.new(@test_path).analyze(7)).to eq 'sivv'
   end
 
   it 'should return "fizzbuzz" for multiples of both 3 and 5' do
-    expect(Fizzbuzzsivv.analyze(15, @test_path)).to eq 'fizzbuzz'
+    expect(Fizzbuzzsivv.new(@test_path).analyze(15)).to eq 'fizzbuzz'
   end
 
   it 'should return "buzzsivv" for multiples of both 5 and 7' do
-    expect(Fizzbuzzsivv.analyze(35, @test_path)).to eq 'buzzsivv'
+    expect(Fizzbuzzsivv.new(@test_path).analyze(35)).to eq 'buzzsivv'
   end
 
   it 'should return "fizzbuzzsivv" for multiples of 3, 5, and 7' do
-    expect(Fizzbuzzsivv.analyze(105, @test_path)).to eq 'fizzbuzzsivv'
+    expect(Fizzbuzzsivv.new(@test_path).analyze(105)).to eq 'fizzbuzzsivv'
   end
 
   it 'should return strings in order of the rules file' do
-    expect(Fizzbuzzsivv.analyze(35, @test_path)).not_to eq 'sivvbuzz'
+    expect(Fizzbuzzsivv.new(@test_path).analyze(35)).to eq 'buzzsivv'
   end
 
   it 'should return input if not a multiple of any of the requested numbers' do
-    expect(Fizzbuzzsivv.analyze(17, @test_path)).to eq 17
+    expect(Fizzbuzzsivv.new(@test_path).analyze(17)).to eq 17
   end
 end
